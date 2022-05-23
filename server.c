87f8e51b9ec8937a81c54941471d4ea81a863766
Tetiana Fedorenko <93830487+tfedorenko@users.noreply.github.com>
2022-05-23 14:46:58 +0200

    Update server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 11:31:03 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/05/20 16:30:35 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/05/23 14:45:38 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,52 @@
 // #include <readline/readline.h>
 // #include <readline/history.h>
 
-
-void signal_handler(int n)
+void	signal_handler(int n)
 {
+	static int	i;
+	static char	c;
+
+	//i = 0;
 	if (n == SIGUSR1)
 	{
-		ft_printf("Siemanko\n");
+		if (i < 8)
+		{
+			c = c + (1 << i);
+			i++;
+		}
+		if (i == 8)
+		{
+			ft_printf("%c", c);
+			c = 0;
+			i = 0;
+		}
+	}
+	else
+	{
+		if (i < 8)
+		{
+			c = c + (0 << i);
+			i++;
+		}
+		if (i == 8)
+		{
+			ft_printf("%c", c);
+			c = 0;
+			i = 0;
+		}
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int pid = getpid();
+	int	pid;
+
+	pid = getpid();
 	ft_printf("PID : %d \n", pid);
 	signal(SIGUSR1, signal_handler);
-	while(1)
+	signal(SIGUSR2, signal_handler);
+	while (1)
 	{
-
 		sleep(1);
 	}
 	return (0);
